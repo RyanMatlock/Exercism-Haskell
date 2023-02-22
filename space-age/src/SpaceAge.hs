6e4f06320fa2e6051fb9b17fcb7a3598fe757103
Ryan Matlock <matlock@matlockmbp.local>
2023-02-22 15:49:34 -0800

    :construction: implement ageOn; pass earth test case

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -8,6 +8,19 @@ data Planet = Mercury
             | Saturn
             | Uranus
             | Neptune
+  deriving (Eq)
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds
+  | planet == Mercury = 0.2408467 * secondsToEarthYears seconds
+  | planet == Venus   = 0.61519726 * secondsToEarthYears seconds
+  | planet == Earth   = secondsToEarthYears seconds
+  | planet == Mars    = 1.8808158 * secondsToEarthYears seconds
+  | planet == Jupiter = 11.862615 * secondsToEarthYears seconds
+  | planet == Saturn  = 29.447498 * secondsToEarthYears seconds
+  | planet == Uranus  = 84.016846 * secondsToEarthYears seconds
+  | planet == Neptune = 164.79132 * secondsToEarthYears seconds
+  | otherwise         = error "not implemented"
+  where
+    secondsToEarthYears sec = sec / 31557600
+
