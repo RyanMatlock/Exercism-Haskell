f0686a0b43b55cca9dc7a8501a34d69fed2d7f0a
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-05 01:14:22 -0800

    :bulb: write note to future self about case; delete commented code

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -8,24 +8,9 @@ data Planet = Mercury
             | Saturn
             | Uranus
             | Neptune
-  -- deriving (Eq) -- necessary for guard version
 
--- Note to future self: div is integral division; / is float division
--- ageOn :: Planet -> Float -> Float
--- ageOn planet seconds
---   | planet == Mercury = secondsToEarthYears seconds / 0.2408467
---   | planet == Venus   = secondsToEarthYears seconds / 0.61519726
---   | planet == Earth   = secondsToEarthYears seconds
---   | planet == Mars    = secondsToEarthYears seconds / 1.8808158
---   | planet == Jupiter = secondsToEarthYears seconds / 11.862615
---   | planet == Saturn  = secondsToEarthYears seconds / 29.447498
---   | planet == Uranus  = secondsToEarthYears seconds / 84.016846
---   | planet == Neptune = secondsToEarthYears seconds / 164.79132
---   | otherwise         = error "not implemented"
---   where
---     secondsToEarthYears sec = sec / 31557600
--- works!
-
+-- using case of is the right way to do this because the compiler forces you to
+-- check for all cases
 ageOn :: Planet -> Float -> Float
 ageOn planet seconds = case planet of
   Mercury -> secondsToEarthYears seconds / 0.2408467
@@ -36,7 +21,6 @@ ageOn planet seconds = case planet of
   Saturn  -> secondsToEarthYears seconds / 29.447498
   Uranus  -> secondsToEarthYears seconds / 84.016846
   Neptune -> secondsToEarthYears seconds / 164.79132
-  -- _       -> error "not implemented"
   where
     secondsToEarthYears sec = sec / 31557600
--- works and doesn't require modifying Planet
+
