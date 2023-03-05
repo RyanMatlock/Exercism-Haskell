ef7112d520ae8204f210b9ae62484566dea82174
Ryan Matlock <ryan.matlock@gmail.com>
2023-03-05 01:46:43 -0800

    :shrug: not sure why this doesn't work (or even compile)
    I need to read up on Haskell more before diving in like this

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -1,9 +1,11 @@
 module Grains (square, total) where
 
+import Data.Maybe (fromMaybe)
+
 square :: Integer -> Maybe Integer
 square n
   | n >= 1 && n <= 64 = Just $ 2 ^ (n - 1)
   | otherwise = Nothing
 
 total :: Integer
-total = error "You need to implement this function."
+total = sum $ map fromMaybe $ map square [1..64]
