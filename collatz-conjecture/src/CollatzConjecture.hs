3762630343a88d7bd37f25d0bafa1e53a6f0776c
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-13 17:34:19 -0700

    ✅ pass all tests

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,10 @@
 module CollatzConjecture (collatz) where
 
+import Data.Maybe (fromMaybe)
+
 collatz :: Integer -> Maybe Integer
-collatz n = error "You need to implement this function."
+collatz n
+  | n < 1 = Nothing
+  | n == 1 = Just 0
+  | mod n 2 == 0 = Just $ succ $ fromMaybe 0 $ collatz $ n `div` 2
+  | otherwise = Just $ succ $ fromMaybe 0 $ collatz $ (3 * n) + 1
