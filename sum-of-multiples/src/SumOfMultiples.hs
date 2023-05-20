88aad4b8393d8839bafbdc92d5a40057b7dea17e
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-20 03:20:07 -0700

    ✅ pass all tests

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,14 @@
 module SumOfMultiples (sumOfMultiples) where
 
+import Data.List (union)
+
+multiplesBelow :: Integer -> Integer -> [Integer]
+multiplesBelow _ 0 = [0]
+multiplesBelow limit n = takeWhile (\x -> x < limit) $ map ((*) n) [1..]
+
+uniqueMultiples :: [[Integer]] -> [Integer]
+uniqueMultiples nss = foldl union [] nss
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit =
+  sum $ uniqueMultiples $ map (multiplesBelow limit) factors
