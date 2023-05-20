1a193d53ddb5a8d9c1ba6bc5464bd6d5df8110e0
Ryan Matlock <ryan.matlock@gmail.com>
2023-05-20 16:17:57 -0700

    :recycle: implement recommended hls changes

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,14 +1,12 @@
 module SumOfMultiples (sumOfMultiples) where
-
 import Data.List (union)
 
 multiplesBelow :: Integer -> Integer -> [Integer]
-multiplesBelow _ 0 = [0]
-multiplesBelow limit n = takeWhile (\x -> x < limit) $ map ((*) n) [1..]
-
-uniqueMultiples :: [[Integer]] -> [Integer]
-uniqueMultiples nss = foldl union [] nss
+multiplesBelow _ 0 = []
+multiplesBelow limit n = takeWhile (limit >) $ map (n *) [1..]
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit =
   sum $ uniqueMultiples $ map (multiplesBelow limit) factors
+  where
+    uniqueMultiples = foldl union []
